c5fa32b35eac742c9d35f4486e3491fc75c8713d
jagadishsetti <47610478+jagadishsetti@users.noreply.github.com>
2019-03-06 22:50:42 -0600

    calculate the salary based on the base

---
+++ Salary.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+#echo "Enter BasiC:"
+#read b
+
+b= 1000
+dp=` expr $b \* 50 / 100 `
+#da=` expr ($b + $dp) \* 35 / 100 `
+bdp=` expr $b + $dp `
+da=` expr $bdp \* 35 / 100 `
+hra=` expr $bdp \* 8 / 100 `
+ma=` expr $bdp \* 3 / 100 `
+pf=` expr $bdp \* 10 / 100 `
+
+Sal=` expr $b + $dp + $da + $hra + $ma - $pf `
+
+#echo "Dp: $dp" 
+#echo "Da: $da" 
+echo "Salary based on the Basic: $Sal" 
